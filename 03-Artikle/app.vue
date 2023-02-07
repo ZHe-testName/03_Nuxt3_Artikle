980ae5a27ffe0bcd20a746a7ca8828e9249466c7
Eugene Xor <horunzhy.wgen@gmail.com>
2023-02-07 21:26:18 +0200

    Finish with composables

---
+++ 03-Artikle/app.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+  const { isDarkMode } = useDarkMode();
+</script>
+
+<template>
+  <div
+    :style="isDarkMode ? {backgroundColor: 'black'} : undefined"
+  >
+    <Nav />
+    <Heading />
+    <Cards />
+  </div>
+</template>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  font-family: Georgia;
+}
+</style>
